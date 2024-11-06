df651fd4fbdd036223cacaca1a24804cc082e316
alaa smadi <alaasmadi1010@gmail.com>
2024-11-06 04:29:46 +0300

    feat(CP): refactor CP UI
    USe CP standard UI

---
+++ control/content/js/index.js
@@ -0,0 +1,84 @@
+/// create a new instance of the buildfire carousel editor
+const editor = new buildfire.components.carousel.editor("#contentCarousel");
+
+/// handle the loading
+function loadItems(carouselItems) {
+	// create an instance and pass it the items if you don't have items yet just pass []
+	editor.loadItems(carouselItems);
+}
+
+/// call buildfire datastore to see if there are any previously saved items
+buildfire.datastore.get(function (err, obj) {
+	if (err)
+		console.error('error');
+	else
+		loadItems(obj.data.carouselItems)
+});
+
+/// save any changes in items
+function save(items) {
+	buildfire.datastore.save({ carouselItems: items }, function (e) {
+		if (e)
+			console.error("error");
+		else
+			console.log('saved.');
+	});
+}
+
+// // this method will be called when a new item added to the list
+editor.onAddItems = editor.onDeleteItem = editor.onItemChange = editor.onOrderChange = function () {
+	save(editor.items);
+};
+
+//Register push notifications event
+const load = () => {
+	for (let propName in analyticKeys) {
+		buildfire.analytics.registerEvent(
+			analyticKeys[propName],
+			{
+				silentNotification: true
+			}
+		);
+	}
+}
+
+let timerDelay = null;
+tinymce.init({
+selector: "#wysiwygContent",
+setup: (editor) => {
+	editor.on("change keyUp", (e) => {
+	if (timerDelay) clearTimeout(timerDelay);
+	timerDelay = setTimeout(() => {
+		let wysiwygContent = tinymce.activeEditor.getContent();
+		saveWYSContent(wysiwygContent);
+	}, 500);
+	});
+	editor.on("init", () => {
+			// Check datastore for previous content
+			buildfire.datastore.get("wysContent", (err, result) => {
+				if (err) return console.error("Error while getting your data", err);
+				if (result && result.data && result.data.content) {
+					console.log("Content found in datastore", result);
+					editor.setContent(result.data.content);
+				}
+			});
+	});
+}
+});
+
+// Save content to datastore
+const saveWYSContent = (content) => {
+	if (content) {
+		buildfire.datastore.save(
+			{ content: content },
+			"wysContent",
+			(err, result) => {
+				if (err) return console.error("Error while saving your data", err);
+			}
+		);
+	}
+}
+
+window.onload = () => {
+	load();
+}
---
+++ control/settings/js/index.js
@@ -0,0 +1,264 @@
+var settings = null;
+var ftqFeautreItems = [];
+var ftqFeaturesContainer = document.getElementById("ftqFeaturesContainer")
+var honorSystemRadioBtn = document.getElementById("honorSystem")
+var scoreFromFreeTextQuestionnaireadioBtn = document.getElementById("scoreFromFreeTextQuestionnaire")
+
+var dailyChange = document.getElementById("dailyChange")
+var weeklyChange = document.getElementById("weeklyChange")
+var monthlyChange = document.getElementById("monthlyChange")
+var allTimeChange = document.getElementById("allTimeChange")
+var enableCalculatePoints = document.getElementById("enableCalculatePoints")
+
+var ftqFeatures = null
+//Register push notifications event
+const load = () => {
+	for (let propName in analyticKeys) {
+		buildfire.analytics.registerEvent(
+			analyticKeys[propName],
+			{
+				silentNotification: true
+			}
+		);
+	}
+	initFeatureActions();
+	initNotificationFrequencyCheckboxes();
+	initUserEarnPointsRadioBtns();
+	initSettings();
+}
+
+
+const initSettings = () => {
+	buildfire.datastore.get("Settings", function (err, result) {
+		if (err || !result) {
+			console.error(err, 'Error while getting datastore.');
+			buildfire.dialog.toast({
+				message: "Error while loading data. Retrying again in 2 seconds...",
+				duration: 1999
+			});
+			setTimeout(function () {
+				location.reload();
+			}, 2000);
+			return;
+		}
+		if (result && result.data) {
+			settings = result.data
+
+			if (settings.userEarnPoints && settings.userEarnPoints == scoreFromFreeTextQuestionnaireadioBtn.value) {
+				scoreFromFreeTextQuestionnaireadioBtn.setAttribute("checked", true)
+			} else {
+				ftqFeaturesContainer.style.display = "none"
+				honorSystemRadioBtn.setAttribute("checked", true)
+			}
+
+			if (settings.calculateLoyaltyPoints && settings.calculateLoyaltyPoints == true) {
+				enableCalculatePoints.checked = true
+			}
+
+			if (settings.features && settings.features.length > 0) {
+				ftqFeatures.loadItems(settings.features);
+				changeActionItemsIcon()
+			} else {
+				settings.features = []
+			}
+
+			if (settings.notificationsFrequency && settings.notificationsFrequency.length > 0) {
+				settings.notificationsFrequency.forEach(notificationFrequency => {
+					if (notificationFrequency == dailyChange.value) {
+						dailyChange.checked = true
+					} else if (notificationFrequency == weeklyChange.value) {
+						weeklyChange.checked = true
+					} else if (notificationFrequency == monthlyChange.value) {
+						monthlyChange.checked = true
+					} else if (notificationFrequency == allTimeChange.value) {
+						allTimeChange.checked = true
+					}
+				});
+			} else {
+				settings.notificationsFrequency = []
+			}
+		}
+	})
+}
+
+const setEnableCalculatePoints = () => {
+	if (enableCalculatePoints.checked) {
+		settings.calculateLoyaltyPoints = true
+	} else {
+		settings.calculateLoyaltyPoints = false
+	}
+	save();
+}
+
+const changeActionItemsIcon = () => {
+	Array.from(document.querySelectorAll(".btn-icon.btn-delete-icon")).forEach(
+		(el) => {
+			el.classList.remove("btn-icon", "btn-delete-icon", "btn-danger");
+			el.classList.add("icon", "icon-cross2");
+		}
+	);
+}
+
+const initUserEarnPointsRadioBtns = () => {
+	scoreFromFreeTextQuestionnaireadioBtn.addEventListener('input', function (evt) {
+		if (settings && settings.userEarnPoints != evt.target.value) {
+			settings.userEarnPoints = evt.target.value;
+			ftqFeaturesContainer.style.display = "block"
+			save();
+		}
+	});
+	honorSystemRadioBtn.addEventListener('input', function (evt) {
+		if (settings && settings.userEarnPoints != evt.target.value) {
+			settings.userEarnPoints = evt.target.value;
+			ftqFeaturesContainer.style.display = "none"
+
+			save();
+		}
+	});
+}
+
+const initNotificationFrequencyCheckboxes = () => {
+	dailyChange.addEventListener('input', function (evt) {
+		if (evt.target.checked) {
+			settings.notificationsFrequency.push(evt.target.value)
+		} else {
+			settings.notificationsFrequency = settings.notificationsFrequency.filter(x => x != evt.target.value)
+		}
+		save();
+
+	});
+	weeklyChange.addEventListener('input', function (evt) {
+		if (evt.target.checked) {
+			settings.notificationsFrequency.push(evt.target.value)
+		} else {
+			settings.notificationsFrequency = settings.notificationsFrequency.filter(x => x != evt.target.value)
+		}
+		save();
+
+	});
+	monthlyChange.addEventListener('input', function (evt) {
+		if (evt.target.checked) {
+			settings.notificationsFrequency.push(evt.target.value)
+		} else {
+			settings.notificationsFrequency = settings.notificationsFrequency.filter(x => x != evt.target.value)
+		}
+		save();
+
+	});
+	allTimeChange.addEventListener('input', function (evt) {
+		if (evt.target.checked) {
+			settings.notificationsFrequency.push(evt.target.value)
+		} else {
+			settings.notificationsFrequency = settings.notificationsFrequency.filter(x => x != evt.target.value)
+		}
+		save();
+	});
+}
+
+const initFeatureActions = () => {
+	ftqFeatures = new buildfire.components.actionItems.sortableList(
+		".ftqFeatures"
+	);
+	document.querySelector(".add-new-item").innerHTML = "Add Feature"
+
+	ftqFeatures.onAddItems = function (item) {
+		item.order = settings.features.length;
+		settings.features.push(item)
+		buildfire.datastore.save(settings, "Settings", function (err, result) {
+			if (err || !result) {
+				console.error("Error saving the widget details: ", err);
+			}
+		});
+
+		changeActionItemsIcon()
+	};
+
+	ftqFeatures.onDeleteItem = function (item, index) {
+		settings.features.splice(index, 1);
+		save();
+	};
+
+	ftqFeatures.onOrderChange = function (item, oldIndex, newIndex) {
+		var items = settings.features;
+
+		items[oldIndex].order = newIndex;
+
+		var tmp = items[oldIndex];
+
+		if (oldIndex < newIndex) {
+			for (var i = oldIndex + 1; i <= newIndex; i++) {
+				items[i - 1] = items[i];
+			}
+		} else {
+			for (var i = oldIndex - 1; i >= newIndex; i--) {
+				items[i + 1] = items[i];
+			}
+		}
+
+		items[newIndex] = tmp;
+
+		settings.features = items;
+		save();
+	};
+
+	ftqFeatures.onItemChange = (item, index) => {
+		settings.features[index] = item;
+		save();
+	};
+
+	let ftqContainer = ftqFeatures.selector.childNodes[0]
+	ftqContainer.removeChild(ftqContainer.childNodes[0])
+	ftqContainer.childNodes[0].classList.remove("col-md-9")
+	ftqContainer.childNodes[0].classList.add("col-md-12")
+
+	let ftqButton = ftqContainer.childNodes[0].childNodes[0]
+	ftqButton.childNodes[0].style.float = "right !important"
+	ftqButton.childNodes[0].classList.remove("pull-left")
+	ftqButton.childNodes[0].classList.add("pull-right")
+}
+
+
+const save = () => {
+	buildfire.datastore.save(settings, "Settings", function (err, result) {
+		if (err || !result) {
+			console.error("Error saving the widget details: ", err);
+		}
+	});
+}
+
+//reset all leaderboards
+const reset = () => {
+	buildfire.dialog.confirm(
+		{
+			title: "Reset Leaderboard",
+			message: "Are you sure you want to reset leaderboard? This will delete all entries and scores in the leaderboard!",
+			confirmButton: {
+				text: "Reset",
+				type: "danger",
+			},
+			cancelButtonText: "Cancel",
+		},
+		(err, isConfirmed) => {
+			if (err) console.error(err);
+
+			if (isConfirmed) {
+				UserSettings.get((err, settings) => {
+					if (err) return console.log(err)
+					if (settings && settings.isSubscribedToPN) {
+						Scores.reset({ isSubscribedToPN: settings.isSubscribedToPN }, (err, res) => {
+							if (err) return console.log(err)
+							buildfire.messaging.sendMessageToWidget("Reset");
+
+							buildfire.dialog.toast({
+								type: "success",
+								message: "Leaderboard has been successfully reset!",
+							});
+						})
+					}
+				})
+			}
+		}
+	);
+}
+
+load()
